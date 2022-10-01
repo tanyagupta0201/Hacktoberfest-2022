1f52f33d0b59c57cfdf3752af62afec4c0784c25
Akshat Mittal <akshatmittal2506@gmail.com>
2022-10-02 00:21:56 +0530

    created complete program for int_roman

--- C++/Integer_Roman.cpp
+++ C++/Integer_Roman.cpp
@@ -1,3 +1,14 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#define _ ios_base::sync_with_stdio(0);
+#define fs cin.tie(0);
+#define ll long long int
+#define pb push_back
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define sortall(v) sort(v.begin(), v.end())
+#define sortrev(v) sort(v.begin(), v.end(), greater<ll>())
+using namespace std;
+
 int romanToInt(string s)
 {
     int n = s.length();
@@ -71,4 +82,22 @@ string intToRoman(int num)
         s /= 10;
     }
     return ans;
+}
+
+void solve()
+{
+    int n;
+    string s;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Roman equivalent of " << n << " is " << intToRoman(n) << endl;
+    cout << "Enter a roman number: ";
+    cin >> s;
+    cout << "Integer equivalent of " << s << " is " << romanToInt(s) << endl;
+}
+
+int main()
+{
+    solve();
+    return 0;
 }
