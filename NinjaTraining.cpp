95276ad6e46dccc8341732db846c5ea4ba03862a
Jai Sinha <jaioff02@gmail.com>
2022-10-01 07:40:13 +0300

    A Leetcode hard problem on Dynamic Programming

--- NinjaTraining.cpp
+++ NinjaTraining.cpp
@@ -1,10 +1,10 @@
 /*
 Name : Jai Sinha
-Date : 01 September 2022*/
+Date : 01 October 2022*/
 
 int ninjaTraining(int n, vector<vector<int>> &points)
 {
-    // Write your code here.
+    // Base Cases.
     vector<vector<int>> dp(n, vector<int>(4, 0));
     dp[0][0]=max(points[0][1],points[0][2]);
     dp[0][1]=max(points[0][2],points[0][0]);
@@ -19,10 +19,10 @@ int ninjaTraining(int n, vector<vector<int>> &points)
       for(int k=0;k<3;k++)
       {
         if (i != k)
-            maxi = max(maxi,dp[j - 1][k]+points[j][k]);
+            maxi = max(maxi,dp[j - 1][k]+points[j][k]);  //max element with points
       }
         dp[j][i]=maxi;
     }
    }
-    return dp[n-1][3];
+    return dp[n-1][3];  //last line of the dp table
 }
