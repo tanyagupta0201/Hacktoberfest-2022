556fcb939357ad4ed217fb46d110c7704f055b65
Jai Sinha <jaioff02@gmail.com>
2022-10-01 07:25:12 +0300

    Cpp-contribution

---
+++ NinjaTraining.cpp
@@ -0,0 +1,28 @@
+/*
+Name : Jai Sinha
+Date : 01 September 2022*/
+
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    // Write your code here.
+    vector<vector<int>> dp(n, vector<int>(4, 0));
+    dp[0][0]=max(points[0][1],points[0][2]);
+    dp[0][1]=max(points[0][2],points[0][0]);
+    dp[0][2]=max(points[0][1],points[0][0]);
+    dp[0][3]=max(points[0][1],max(points[0][0],points[0][2]));
+    for(int j=1;j<n;j++)
+    {
+    for(int i=0;i<=3;i++)
+    {
+        dp[j][i]=0;
+        int maxi=0;
+      for(int k=0;k<3;k++)
+      {
+        if (i != k)
+            maxi = max(maxi,dp[j - 1][k]+points[j][k]);
+      }
+        dp[j][i]=maxi;
+    }
+   }
+    return dp[n-1][3];
+}
