84ce391f2928785b7880e61a3f1f1279e25e24eb
mohit goyal <mohitgoyalbxn@gmail.com>
2022-10-01 11:11:55 +0530

    added fenwick tree

---
+++ C++/fenwick tree.cpp
@@ -0,0 +1,68 @@
+//Author name : Mohit Kumar Goyal
+//Modify date : 1st oct.
+
+
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct fenwick {
+	vector<int> fn;
+	int n;
+
+	void init(int n) {
+		this->n = n + 1;
+		fn.resize(this->n, 0);
+	}
+
+	void add(int x, int y) {
+		x++; // 1 based index
+		while (x < n) {
+			fn[x] += y;
+			x += (x & (-x));// last set bit
+		}
+	}
+
+	// void add(int x, int y) {
+	// 	for (x++; x < n; x += (x & (-x))) fn[x] += y;
+	// }
+
+	int sum(int x) {
+		x++; // 1 based index
+		int ans = 0;
+		while (x) {
+			ans += fn[x];
+			x -= (x & (-x));// last set bit
+		}
+		return ans;
+	}
+
+	int sum(int l, int r) {
+		return sum(r) - sum(l - 1);
+	}
+
+};
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+	fenwick tree;
+	tree.init(v.size());
+
+	for (int i = 0; i < v.size(); i++) {
+		tree.add(i, v[i]);
+	}
+
+	cout << tree.sum(3, 5) << '\n';
+
+	tree.add(4, -3);
+
+	cout << tree.sum(3, 5) << '\n';
+
+	return 0;
+}
