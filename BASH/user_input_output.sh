d3a697278eea01b6358a7b3a2aa80445e2fec479
yunocrown <jatin2002sep@gmail.com>
2022-10-01 16:12:45 +0530

    creating bash directory

---
+++ BASH/user_input_output.sh
@@ -0,0 +1,12 @@
+
+#    Authors Name : Jatin Saini
+#    Date Modified: 01 October, 2022
+
+#!/bin/bash
+
+echo "What is your name?"
+read name
+
+echo "Hi there $name"
+echo "Welcome to the contributions of Bash directory!"
+
