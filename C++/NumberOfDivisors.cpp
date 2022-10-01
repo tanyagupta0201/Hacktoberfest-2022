579d97a949b862f2c763bda41e80a4da8e2be13f
Anamika Pandey <83291071+anamika62@users.noreply.github.com>
2022-10-02 00:46:49 +0530

    Create NumberOfDivisors.cpp

---
+++ C++/NumberOfDivisors.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,count=0;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                if(i%3==0)
+                {
+                    count++;
+                }
+            }
+        }
+        cout<<count<<endl;
+    }
+    return 0;
+
+}C
